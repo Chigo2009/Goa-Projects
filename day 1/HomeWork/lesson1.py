de7a00724fe4840b7ba9622986ab94b742ef08a0
Chigo2009 <chigvinadzegiorgi2009@gmail.com>
2024-09-01 18:16:31 +0400

    first commit

---
+++ day 1/HomeWork/lesson1.py
@@ -0,0 +1,12 @@
+name = "Giorgi"
+surname= "Chigvinadzew"
+age= 14
+height= 174
+
+knows_programmin = True
+
+
+
+print("My name is "+name+" "+" my surname is "+surname+" "+"i am "+" "+str(age)+" "+"years old" " my height is"+str(height))
+
+

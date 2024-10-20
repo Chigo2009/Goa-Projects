af91c9b52efb2526b82fd964edee9195c676acd6
Chigo2009 <chigvinadzegiorgi2009@gmail.com>
2024-10-20 16:29:16 +0400

    commit

---
+++ day 10/HomeWork/pyton.py
@@ -0,0 +1,27 @@
+age = int(input("Enter your age: "))
+for i in range ((age)):
+   
+    print("your age is: ",  (i))
+temperatureC = int(input("Enter your temperatureC: "))
+temperatureF= (9 / 5 * temperatureC + 32)
+print("Your temperature in F is: " , temperatureF)
+a = 10
+b = 10
+print(a == b)
+x = 5
+y = 3
+print(x != y)
+m = 7
+n = 4
+print(m > n)
+a = True
+b = False
+print(a and b)
+x = True
+y = False
+print(x or y)
+condition = True
+print(not condition)
+
+
+

e7ed386b761dab91bdeeb046be384a8fd99655a4
Chigo2009 <chigvinadzegiorgi2009@gmail.com>
2024-11-11 20:31:39 +0400

    comment

---
+++ BossLevel/Bosslevel.py
@@ -0,0 +1,7 @@
+arr = [9,5,94,711,52,96,71,8]
+min_number = arr[0]
+
+for num in arr:
+    if num < min_number:
+        min_number = num
+print(min_number)
---
+++ day 18/HomeWork/pyton.py
@@ -0,0 +1,30 @@
+favorite_fruits = ["ვაშლი", "ბანანი", "თამარა", "ანგური", "გარგარი"]
+
+print("პირველი ხილი:", favorite_fruits[0])
+
+print("ბოლო ხილი:", favorite_fruits[-1])
+
+numbers = list(range(1, 11))
+
+
+print("პირველი ელემენტი:", numbers[0])  
+print("მეხუთე ელემენტი:", numbers[4])   
+print("მეათე ელემენტი:", numbers[9])    
+
+animals = ["ძაღლი", "ქათამი", "კატა", "საგასი"]
+
+
+animals[0] = "ლომი"
+
+
+print("განახლებული სია:", animals)
+
+favorite_movies = ["ინტერნეითი", "მზის მოშორება", "შპიონი", "სამეფო გრძნობა"]
+
+
+print("ბოლო ფილმი:", favorite_movies[-1])
+numbers = [10, 20, 30, 40, 50, 60]
+
+
+print("უკანასკნელი ორი ელემენტი:", numbers[-2:], )
+

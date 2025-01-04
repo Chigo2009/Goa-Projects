f4aa5348cb000529c0dc503d1b876b2af9578640
Chigo2009 <chigvinadzegiorgi2009@gmail.com>
2025-01-04 17:29:53 +0400

    comment

---
+++ day 34/Classwork/py.py
@@ -0,0 +1,5 @@
+
+favorite_movies = ("Inception", "The Matrix", "Interstellar", "The Dark Knight", "Forrest Gump", "The Shawshank Redemption")
+first_movie, *remaining_movies = favorite_movies
+print("First movie:", first_movie)
+print("Remaining movies:", remaining_movies)
